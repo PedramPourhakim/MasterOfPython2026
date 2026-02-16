e740b1135b1f9d3eec02f04be930db01d6499fcd
pedram pourhakim <pedrampourhakim1999@gmail.com>
2026-02-16 16:23:30 +0330

    flask portfolio configuration second

--- Chapter3/Flask/Portfolio/main.py
+++ Chapter3/Flask/Portfolio/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect,url_for
 
 app = Flask(__name__,
             template_folder='templates',
@@ -7,8 +7,16 @@ app = Flask(__name__,
 
 @app.get('/')
 def index():
+
     context = {
+        "message" : request.args.get('message', ""),
         "fullname": "Pedram Pourhakim"
     }
     return render_template('index.html',context= context)
 
+
+
+@app.post('/contact')
+def contact():
+    app.logger.debug(request.form.get('name'))
+    return redirect(url_for('index',message="ticket successfully submitted, I will contact you later."))
